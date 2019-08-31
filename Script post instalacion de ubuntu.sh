77f97b6911b019aa94e4a7607a80aef6fc11dece
mester-ces <52388120+mester-ces@users.noreply.github.com>
2019-08-31 07:48:24 +0200

    Add files via upload

---
+++ Script post instalacion de ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install synaptic arc-theme neofetch gnome-tweaks
+
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt-get update
+sudo apt-get install papirus-icon-theme
